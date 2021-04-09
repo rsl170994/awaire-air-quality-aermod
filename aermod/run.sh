7af227d8f0f4775e6f6438b7832ce04d2d33e41e
eidos <eidos@eidos.com.br>
2021-04-09 02:14:46 -0300

    adding argument inside ''

--- aermod/run.sh
+++ aermod/run.sh
@@ -1,4 +1,4 @@
-python3.9 /var/www/awaire-air-quality-aermod/aermod_input_creator/aermod_input_creator_r01.py $1
+python3.9 /var/www/awaire-air-quality-aermod/aermod_input_creator/aermod_input_creator_r01.py '$1'
 ./aermet.exe s1.inp
 ./aermet.exe s2.inp
 ./aermet.exe s3.inp
